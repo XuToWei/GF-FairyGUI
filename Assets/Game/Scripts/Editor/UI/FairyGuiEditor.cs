51ea79235458e376a2ec68da87391b2a5ad80ade
XuToWei <xw741619720@qq.com>
2023-11-01 20:59:48 +0800

    去掉UGUI代码

--- Assets/Game/Scripts/Editor/UI/FairyGuiEditor.cs
+++ Assets/Game/Scripts/Editor/UI/FairyGuiEditor.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using FairyGUI;
-using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
 using Object = UnityEngine.Object;
--- Assets/Game/Scripts/UI/Base/UIExtension.cs
+++ Assets/Game/Scripts/UI/Base/UIExtension.cs
@@ -2,41 +2,12 @@
 using GameFramework.UI;
 using System.Collections;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityGameFramework.Runtime;
 
 namespace Game
 {
     public static class UIExtension
     {
-        public static IEnumerator FadeToAlpha(this CanvasGroup canvasGroup, float alpha, float duration)
-        {
-            float time = 0f;
-            float originalAlpha = canvasGroup.alpha;
-            while (time < duration)
-            {
-                time += Time.deltaTime;
-                canvasGroup.alpha = Mathf.Lerp(originalAlpha, alpha, time / duration);
-                yield return new WaitForEndOfFrame();
-            }
-
-            canvasGroup.alpha = alpha;
-        }
-
-        public static IEnumerator SmoothValue(this Slider slider, float value, float duration)
-        {
-            float time = 0f;
-            float originalValue = slider.value;
-            while (time < duration)
-            {
-                time += Time.deltaTime;
-                slider.value = Mathf.Lerp(originalValue, value, time / duration);
-                yield return new WaitForEndOfFrame();
-            }
-
-            slider.value = value;
-        }
-
         public static bool HasUIForm(this UIComponent uiComponent, int uiFormId, string uiGroupName = null)
         {
             IDataTable<DRUIForm> dtUIForm = GameEntry.DataTable.GetDataTable<DRUIForm>();
